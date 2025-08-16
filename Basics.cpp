ca2cde831e266b21e7be865062553226d4061609
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-08-16 22:01:48 +0530

    files added

---
+++ Basics.cpp
@@ -0,0 +1,108 @@
+// #include<iostream>
+// #include<math.h>
+// #include<string>
+
+// so these are the libraries each for I/O operations , for math functions and for string respectively
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     std::cout << "hey string"<<std::endl;
+//     std::cout << "hey string"<<"\n";
+//     return 0;
+// }
+
+// ----------------
+// using namespace std;
+// int main(){
+//     int x , y;
+//     cin>>x>>y; //taking input
+//     cout<<"value of " << x << y;
+//     return 0;
+// }
+
+// DATA TYPES 
+
+// 1.int (4 bytes)
+// 2.unsigned short (2 bytes ){0 -->> 65,535}
+// long (4 bytes)
+// unsigned long ( 4 bytes)
+// long long (8 bytes)
+// float (4 byte)
+// double (8 byte)
+// long double (10 byte)
+
+
+// string and getline
+
+// string ---
+// int main(){
+//     string s1;
+//     string s2 ;
+//     cin >> s1 >>s2;
+//     cout<<s1<<" "<<s2;
+//     return 0;
+// }
+// word after space is considered as new string
+// always in double quotes 
+
+// if we want to group strings and bring it to line ,use getline (internal function)
+
+// int main(){
+//     string str;
+//     getline(cin , str);
+//     cout << str;
+//     return 0;
+// }
+
+// exmaple : input :hey i am this 
+                    // ohhh
+
+                    // the first line will be printed and second doesn't due to termination
+
+// CHAR : there are 256 charcters in english dictionary ,it can store all of them 
+
+// in single quotes
+
+// int main(){
+//     char ch ;
+//     cin>>ch;
+//     cout <<ch;
+//     return 0;
+// }
+
+// IF-ELSE STATEMENT
+
+/*
+int main(){
+    int age;
+    cin >> age;
+    if(age > 18){
+        cout << "you are an adult ";
+        
+    }else{
+        cout << "you are not an adult ";
+    }
+    return 0;
+}
+*/
+
+// problem solution :
+
+// int main(){
+//     int marks ;
+//     cin >> marks;
+//     if(marks < 25){
+//         cout << "F"
+//     }
+//     else if (marks >= 25 && marks <=44){
+//         cout << "E";
+//     }
+//     else{
+//         cout <<" fail";
+//     }
+
+// }
+
+// SWITCH Statement
---
+++ space-time.cpp
@@ -0,0 +1,42 @@
+// space and time complexity
+
+// TIME COMPLEXITY
+// we can't  say !
+
+// time_complexity != time taken by program to execute
+
+// time complxity is the rate at which the time taken increases with respect to the input size
+
+// T.C = represnted by BIG-O notation -->> O(time taken hypothetically )
+
+// for(i=1 ; i<=5 ; i++){
+//     cout << "hii";
+// }
+
+// O() = no. of steps this code takes
+
+// the loop runs for 5 times 
+// every time there are 3 operations performed :
+// increment --> check --> print 
+
+// T.C = O(5 ✖️ 3) = O(15)
+// but this is a number ,we can't represent T.C in number 
+// so for i<=N 
+// the code will run  for N time 
+// and the op. performed = 3
+//  so the T.C = O(N ✖️ 3)
+
+// 3 Rules to remembered while calculating Time complexity :
+// 1.Always compute the T.C in worst case scenario
+// 2.Avoid constant
+// 3.avoid lower value
+
+// SPACE COMPLEXITY
+// it is defined as the sum of 
+
+// AUXILLARY SPACE + INPUT SPACE 
+
+// AUXILLARY SPACE : space that we take to solve the problem 
+// INPUT SPACE : the space tha we take to store the input 
+
+// I RULE : Never manipulate or temper the data unless and untill it is being told by the interviewer
